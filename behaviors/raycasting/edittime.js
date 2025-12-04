0b90857c7ac3e6b0ae5b37b2cead5ddc32e23bd2
pigmalien <japigg@bellsouth.net>
2025-12-04 18:03:55 -0500

    Enhance Raycasting behavior: update version, improve collision detection, and add expressions for hit details

--- behaviors/raycasting/edittime.js
+++ behaviors/raycasting/edittime.js
@@ -1,44 +1,86 @@
 ﻿function GetBehaviorSettings()
 {
 	return {
-		"name":			"Raycasting",
+		"name":			"Raycasting",			// as appears in 'add behavior' dialog, can be changed as long as "id" stays the same
 		"id":			"Raycasting",
-		"version":		"0.1",
-		"description":	"A minimal test behavior to see if new plugins can load.",
+		"version":		"1.0",
+		"description":	"Fires a ray from the object's origin and checks for collision with specific objects.",
 		"author":		"Gemini Code Assist",
 		"help url":		"",
-		"category":		"Test",
+		"category":		"General",
 		"flags":		0
 	};
 };
 
 ////////////////////////////////////////
 // Actions
-AddAction(0, af_none, "Log message", "Test", "Log 'Raycasting loaded' to console", "A test action to verify the plugin loads.", "LogMessage");
+AddNumberParam("Angle", "The angle in degrees to cast the ray.", "0");
+AddNumberParam("Distance", "The maximum distance in pixels for the ray.", "1000");
+AddObjectParam("Objects", "The object type to check for collision.");
+AddAction(0, af_none, "Cast ray", "Raycasting", "Cast ray at angle {0} for distance {1} against {2}", "Fires a ray and checks for the first collision with a target object.", "CastRay");
+
+////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On ray hit", "Raycasting", "On ray hit", "Triggered when a ray cast hits an object.", "OnRayHit");
+AddCondition(1, cf_none, "Ray did hit", "Raycasting", "Ray did hit", "True if the last ray cast resulted in a hit.", "DidHit");
+
+////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "RayHitX", "Raycasting", "RayHitX", "The X coordinate of the ray's collision point.");
+AddExpression(1, ef_return_number, "RayHitY", "Raycasting", "RayHitY", "The Y coordinate of the ray's collision point.");
+AddExpression(2, ef_return_number, "RayHitDistance", "Raycasting", "RayHitDistance", "The distance from the origin to the ray's collision point.");
+AddExpression(3, ef_return_number, "RayHitUID", "Raycasting", "RayHitUID", "The UID of the object instance the ray collided with.");
 
 ////////////////////////////////////////
 ACESDone();
 
 var property_list = [
-	new cr.Property(ept_text, "Test Property", "It works!", "A test property.")
+	// No properties needed for this behavior
 ];
 
-function CreateIDEBehaviorType() { return new IDEBehaviorType(); }
-function IDEBehaviorType() { assert2(this instanceof arguments.callee, "Constructor called as a function"); }
-IDEBehaviorType.prototype.CreateInstance = function(instance, type) { return new IDEInstance(instance, type); }
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
 function IDEInstance(instance, type)
 {
 	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
 	this.instance = instance;
 	this.type = type;
+	
+	// Set the default property values from the property table
 	this.properties = {};
-
+	
 	for (var i = 0; i < property_list.length; i++)
 		this.properties[property_list[i].name] = property_list[i].initial_value;
 }
+
+// Called by the IDE after a property has been changed
 IDEInstance.prototype.OnPropertyChanged = function(property_name)
 {
 }
 
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
 // The runtime script file to include in the exported project
-function GetBehaviorScript() { return "Raycasting_behavior.js"; }
+function GetBehaviorScript() { return "runtime.js"; }
--- behaviors/raycasting/runtime.js
+++ behaviors/raycasting/runtime.js
@@ -1,74 +1,190 @@
 ﻿﻿"use strict";
 assert2(cr, "cr namespace not created");
 assert2(cr.behaviors, "cr.behaviors not created");
-
-/////////////////////////////////////
-// The behavior constructor
-cr.behaviors.Raycasting = function(runtime)
-{
-	this.runtime = runtime;
-};
-
 (function ()
 {
-	var behaviorProto = cr.behaviors.Raycasting.prototype;
-		
-	/////////////////////////////////////
-	// Behavior type class
-	behaviorProto.Type = function(behavior, objtype)
-	{
-		this.behavior = behavior;
-		this.objtype = objtype;
-		this.runtime = behavior.runtime;
-	};
-	
-	var behtypeProto = behaviorProto.Type.prototype;
+    cr.behaviors.Raycasting = function(runtime) {
+        this.runtime = runtime;
+    };
 
-	behtypeProto.onCreate = function()
-	{
-	};
+    var behaviorProto = cr.behaviors.Raycasting.prototype;
 
-	/////////////////////////////////////
-	// Behavior instance class
-	behaviorProto.Instance = function(type, inst)
-	{
-		this.type = type;
-		this.behavior = type.behavior;
-		this.inst = inst;
-		this.runtime = type.runtime;
-	};
-	
-	var behinstProto = behaviorProto.Instance.prototype;
+    behaviorProto.Type = function(behavior, objtype) {
+        this.behavior = behavior;
+        this.objtype = objtype;
+        this.runtime = behavior.runtime;
+    };
 
-	behinstProto.onCreate = function()
-	{
-		this.myProperty = this.properties[0];
-	};
-	
-	behinstProto.saveToJSON = function ()
-	{
-		return { "myprop": this.myProperty };
-	};
-	
-	behinstProto.loadFromJSON = function (o)
-	{
-		this.myProperty = o["myprop"];
-	};
+    var behtypeProto = behaviorProto.Type.prototype;
 
-	behinstProto.tick = function ()
-	{
-		// This function is called every tick.
-	};
+    behtypeProto.onCreate = function() {};
 
-	//////////////////////////////////////
-	// Actions
-	function Acts() {};
-	Acts.prototype.LogMessage = function ()
-	{
-		console.log("Raycasting behavior loaded! Property is: " + this.myProperty);
-	};
-	behaviorProto.acts = new Acts();
+    behaviorProto.Instance = function(type, inst) {
+        this.type = type;
+        this.behavior = type.behavior;
+        this.inst = inst;
+        this.runtime = type.runtime;
+    };
 
-	behaviorProto.cnds = {};
-	behaviorProto.exps = {};
+    var behinstProto = behaviorProto.Instance.prototype;
+
+    behinstProto.onCreate = function() {
+
+        // State for the last ray cast
+        this.didHit = false;
+        this.hitX = 0;
+        this.hitY = 0;
+        this.hitDist = 0;
+        this.hitUID = -1;
+    };
+
+    behinstProto.saveToJSON = function() {
+        return {
+            "didHit": this.didHit,
+            "hitX": this.hitX,
+            "hitY": this.hitY,
+            "hitDist": this.hitDist,
+            "hitUID": this.hitUID
+        };
+    };
+
+    behinstProto.loadFromJSON = function(o) {
+        this.didHit = o["didHit"];
+        this.hitX = o["hitX"];
+        this.hitY = o["hitY"];
+        this.hitDist = o["hitDist"];
+        this.hitUID = o["hitUID"];
+    };
+
+    behinstProto.tick = function() {};
+
+    function Cnds() {};
+    Cnds.prototype.OnRayHit = function() { return true; };
+    Cnds.prototype.DidHit = function() { return this.didHit; };
+    behaviorProto.cnds = new Cnds();
+
+    // Helper function for squared distance, as cr.distanceSqTo is not in the C2 SDK
+    function distanceSqTo(x1, y1, x2, y2) {
+        var dx = x2 - x1;
+        var dy = y2 - y1;
+        return dx * dx + dy * dy;
+    }
+
+    // Helper function to find the intersection point of two line segments
+    // Returns null if they don't intersect, or {x, y} if they do.
+    function line_segment_intersection(x1, y1, x2, y2, x3, y3, x4, y4) {
+        var den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        if (den === 0) {
+            return null; // Lines are parallel or collinear
+        }
+
+        var t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
+        var u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
+
+        if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+            return {
+                x: x1 + t * (x2 - x1),
+                y: y1 + t * (y2 - y1)
+            };
+        }
+
+        return null;
+    }
+
+    // Helper function to check for the closest intersection between a ray and a bounding box
+    function ray_box_intersection(ray_x1, ray_y1, ray_x2, ray_y2, bbox) {
+        var sides = [
+            [bbox.left, bbox.top, bbox.right, bbox.top],       // Top
+            [bbox.right, bbox.top, bbox.right, bbox.bottom],  // Right
+            [bbox.right, bbox.bottom, bbox.left, bbox.bottom], // Bottom
+            [bbox.left, bbox.bottom, bbox.left, bbox.top]      // Left
+        ];
+
+        var closest_hit = null;
+        var closest_dist_sq = Infinity;
+
+        for (var i = 0; i < 4; i++) {
+            var hit = line_segment_intersection(ray_x1, ray_y1, ray_x2, ray_y2, sides[i][0], sides[i][1], sides[i][2], sides[i][3]);
+            if (hit) {
+                var dist_sq = distanceSqTo(ray_x1, ray_y1, hit.x, hit.y);
+                if (dist_sq < closest_dist_sq) {
+                    closest_dist_sq = dist_sq;
+                    closest_hit = hit;
+                }
+            }
+        }
+        return closest_hit;
+    }
+
+    function Acts() {};
+    Acts.prototype.CastRay = function(angle, distance, obj) {
+        if (!obj) return;
+
+        var startX = this.inst.x;
+        var startY = this.inst.y;
+        var rad = cr.to_radians(angle);
+        var endX = startX + Math.cos(rad) * distance;
+        var endY = startY + Math.sin(rad) * distance;
+
+        var candidates = obj.getCurrentSol().getObjects();
+        var closestHit = null;
+        var closestDistSq = Number.MAX_VALUE;
+
+        // Reset state before casting
+        this.didHit = false;
+        this.hitUID = -1;
+
+        for (var i = 0; i < candidates.length; i++) {
+            var inst = candidates[i];
+            if (inst === this.inst) continue; // Don't hit self
+
+            inst.update_bbox();
+            var result = ray_box_intersection(startX, startY, endX, endY, inst.bbox);
+
+            if (result) {
+                var distSq = distanceSqTo(startX, startY, result.x, result.y);
+                if (distSq < closestDistSq) {
+                    closestDistSq = distSq;
+                    closestHit = {
+                        x: result.x,
+                        y: result.y,
+                        inst: inst
+                    };
+                }
+            }
+        }
+
+        if (closestHit) {
+            this.didHit = true;
+            this.hitX = closestHit.x;
+            this.hitY = closestHit.y;
+            this.hitDist = Math.sqrt(closestDistSq);
+            this.hitUID = closestHit.inst.uid;
+
+            // Pick the specific instance that was hit
+            var sol = obj.getCurrentSol();
+            sol.select_all = false;
+            sol.instances.length = 1;
+            sol.instances[0] = closestHit.inst;
+
+            this.runtime.trigger(cr.behaviors.Raycasting.prototype.cnds.OnRayHit, this.inst);
+        }
+    };
+    behaviorProto.acts = new Acts();
+
+    function Exps() {};
+    Exps.prototype.RayHitX = function(ret) {
+        ret.set_float(this.hitX);
+    };
+    Exps.prototype.RayHitY = function(ret) {
+        ret.set_float(this.hitY);
+    };
+    Exps.prototype.RayHitDistance = function(ret) {
+        ret.set_float(this.hitDist);
+    };
+    Exps.prototype.RayHitUID = function(ret) {
+        ret.set_int(this.hitUID);
+    };
+    behaviorProto.exps = new Exps();
+
 }());
