e7db7abbcaed778972f1cb480e95c5fe0f9636a1
pigmalien <japigg@bellsouth.net>
2025-12-11 15:53:41 -0500

    Add initial implementation of MyPlugin with common, edit time, and runtime scripts
    - Created common.js for shared scripts between IDE and runtime.
    - Implemented edittime.js with plugin settings, parameter types, conditions, actions, and expressions.
    - Developed runtime.js with strict mode, plugin class, instance management, and example conditions, actions, and expressions.

---
+++ behaviors/hpmp/edittime.js
@@ -0,0 +1,175 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Rpg Points",			// as appears in 'add behavior' dialog, can be changed as long as "id" stays the same
+		"id":			"RpgPoints",			// this is used to identify this behavior and is saved to the project; never change it
+		"version":		"1.0",					// (float in x.y format) Behavior version - C2 shows compatibility warnings based on this
+		"description":	"A flexible resource manager for HP, MP, and any custom values like AP, Rage, etc.",
+		"author":		"Gemini Code Assist",
+		"help url":		"https://www.construct.net",
+		"category":		"General",				// Prefer to re-use existing categories, but you can set anything here
+		"flags":		0						// uncomment lines to enable flags...
+					//	| bf_onlyone			// can only be added once to an object, e.g. solid
+	};
+};
+
+////////////////////////////////////////
+// Conditions
+
+// On Health Depleted
+AddCondition(0, cf_trigger, "On health depleted", "HP", "{my} On health depleted", "Triggered when the 'HP' resource drops to 0 or below.", "OnHealthDepleted");
+
+// Is Resource Maxed
+AddStringParam("Resource key", "The key for the resource to check (e.g., \"HP\", \"MP\", \"AP\").");
+AddCondition(1, cf_none, "Is resource maxed", "Resources", "{my} Is resource <b>{0}</b> maxed", "True if the current value of a resource is equal to its maximum.", "IsResourceMaxed");
+
+// Is Resource Available
+AddStringParam("Resource key", "The key for the resource to check (e.g., \"HP\", \"MP\", \"AP\").");
+AddNumberParam("Required amount", "The amount required for the check to pass.");
+AddCondition(2, cf_none, "Is resource available", "Resources", "{my} Resource <b>{0}</b> has at least <b>{1}</b>", "True if the current value of a resource is greater than or equal to a specific amount.", "IsResourceAvailable");
+
+// On Mana Depleted
+AddCondition(3, cf_trigger, "On mana depleted", "MP", "{my} On mana depleted", "Triggered when the 'MP' resource drops to 0 or below.", "OnManaDepleted");
+
+////////////////////////////////////////
+// Actions
+
+// Take Damage
+AddNumberParam("Amount", "The amount of damage to take (will be subtracted from 'HP').");
+AddAction(0, af_none, "Take damage", "HP", "Take <b>{0}</b> damage", "Deduct an amount from the 'HP' resource.", "TakeDamage");
+
+// Heal
+AddNumberParam("Amount", "The amount to heal (will be added to 'HP').");
+AddAction(1, af_none, "Heal", "HP", "Heal <b>{0}</b> HP", "Add an amount to the 'HP' resource, capped at its maximum.", "Heal");
+
+// Modify Resource
+AddStringParam("Resource key", "The key for the resource to modify (e.g., \"HP\", \"MP\", \"AP\").");
+AddAnyTypeParam("Amount", "The amount to add (positive) or subtract (negative) from the resource.");
+AddAction(2, af_none, "Modify resource", "Resources", "Modify <b>{0}</b> by <b>{1}</b>", "Add to or subtract from any resource pool.", "ModifyResource");
+
+// Set Max Resource
+AddStringParam("Resource key", "The key for the resource to modify (e.g., \"HP\", \"MP\", \"AP\").");
+AddNumberParam("New maximum", "The new maximum value for the resource.");
+AddAction(3, af_none, "Set max resource", "Resources", "Set max <b>{0}</b> to <b>{1}</b>", "Set the maximum capacity for a resource.", "SetMaxResource");
+
+// Add Custom Resource
+AddStringParam("Resource key", "A unique name for the new resource (e.g., \"RAGE\", \"STAMINA\").");
+AddNumberParam("Maximum amount", "The maximum capacity of this new resource.", "100");
+AddNumberParam("Current amount", "The starting value of this new resource.", "100");
+AddNumberParam("Regen rate", "The amount to regenerate per second (can be negative).", "0");
+AddAction(4, af_none, "Add custom resource", "Resources", "Add custom resource <b>{0}</b> (max: {1}, current: {2}, regen: {3}/s)", "Initialize and register a new custom resource pool.", "AddCustomResource");
+
+// Spend Mana
+AddNumberParam("Amount", "The amount of mana to spend (will be subtracted from 'MP').");
+AddAction(5, af_none, "Spend mana", "MP", "Spend <b>{0}</b> mana", "Deduct an amount from the 'MP' resource.", "SpendMana");
+
+// Restore Mana
+AddNumberParam("Amount", "The amount of mana to restore (will be added to 'MP').");
+AddAction(6, af_none, "Restore mana", "MP", "Restore <b>{0}</b> MP", "Add an amount to the 'MP' resource, capped at its maximum.", "RestoreMana");
+
+////////////////////////////////////////
+// Expressions
+
+// CurrentResource
+AddStringParam("ResourceKey", "The key of the resource to get the current value of.");
+AddExpression(0, ef_return_number, "CurrentResource", "Resources", "CurrentResource", "Returns the current value of the specified resource pool.");
+
+// MaxResource
+AddStringParam("ResourceKey", "The key of the resource to get the maximum value of.");
+AddExpression(1, ef_return_number, "MaxResource", "Resources", "MaxResource", "Returns the maximum capacity of the specified resource pool.");
+
+// ResourcePercent
+AddStringParam("ResourceKey", "The key of the resource to get the percentage of.");
+AddExpression(2, ef_return_number, "ResourcePercent", "Resources", "ResourcePercent", "Returns the current value as a percentage (0-100) of the maximum.");
+
+
+////////////////////////////////////////
+// HP & MP Expressions
+AddExpression(3, ef_return_number, "HP", "HP", "HP", "Returns the current value of the HP resource.");
+AddExpression(4, ef_return_number, "MaxHP", "HP", "MaxHP", "Returns the maximum capacity of the HP resource.");
+AddExpression(5, ef_return_number, "HPPercent", "HP", "HPPercent", "Returns the current HP as a percentage (0-100).");
+
+AddExpression(6, ef_return_number, "MP", "MP", "MP", "Returns the current value of the MP resource.");
+AddExpression(7, ef_return_number, "MaxMP", "MP", "MaxMP", "Returns the maximum capacity of the MP resource.");
+AddExpression(8, ef_return_number, "MPPercent", "MP", "MPPercent", "Returns the current MP as a percentage (0-100).");
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+// Array of property grid properties for this plugin
+// new cr.Property(ept_integer,		name,	initial_value,	description)		// an integer value
+// new cr.Property(ept_float,		name,	initial_value,	description)		// a float value
+// new cr.Property(ept_text,		name,	initial_value,	description)		// a string
+// new cr.Property(ept_combo,		name,	"Item 1",		description, "Item 1|Item 2|Item 3")	// a dropdown list (initial_value is string of initially selected item)
+
+var property_list = [
+	new cr.Property(ept_float, 	"Max HP",		100,		"The primary resource's maximum capacity."),
+	new cr.Property(ept_float, 	"Max MP",		50,			"The secondary resource's maximum capacity."),
+	new cr.Property(ept_float, 	"Default Regen Rate (HP)",		0,		"HP recovered per second."),
+	new cr.Property(ept_float, 	"Default Regen Rate (MP)",		0,		"MP recovered per second.")
+	];
+	
+//////////////////////////////////////////////////////////////
+// The rest of this file is boilerplate logic; you may not need to edit it.
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])			// a number
+// AddStringParam(label, description [, initial_string = "\"\""])		// a string
+// AddAnyTypeParam(label, description [, initial_string = "0"])			// accepts either a number or string
+// AddCmpParam(label, description)										// combo with equal, not equal, less, etc.
+// AddComboParamOption(text)											// (repeat before "AddComboParam" to add combo items)
+// AddComboParam(label, description [, initial_selection = 0])			// a dropdown list parameter
+// AddObjectParam(label, description)									// a button to click and pick an object type
+// AddLayerParam(label, description)									// accepts either a layer number or name (string)
+// AddLayoutParam(label, description)									// a dropdown list with all project layouts
+// AddKeybParam(label, description)										// a button to click and press a key (returns a VK)
+// AddAudioFileParam(label, description)								// a dropdown list with all imported project audio files
+
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// any other properties here, e.g...
+	// this.myValue = 0;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/hpmp/runtime.js
@@ -0,0 +1,308 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+// The behavior ID is "RpgPoints"
+cr.behaviors.RpgPoints = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	// The behavior ID is "RpgPoints"
+	var behaviorProto = cr.behaviors.RpgPoints.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+		// This object will store all resource pools, e.g. this.resources["HP"] = {current: 100, max: 100, regen: 0}
+		this.resources = {};
+		
+		// Initialize default HP and MP from properties
+		var maxHP = this.properties[0];
+		var maxMP = this.properties[1];
+		var regenHP = this.properties[2];
+		var regenMP = this.properties[3];
+
+		// Use the "Add Custom Resource" action's logic to keep initialization consistent
+		this.addResource("HP", maxHP, maxHP, regenHP);
+		this.addResource("MP", maxMP, maxMP, regenMP);
+
+		// Enable the tick function if any resource has regeneration
+		this.runtime.tickMe(this);
+	};
+
+	// A helper function to add/update a resource and clamp its values
+	behinstProto.addResource = function(key, max, current, regen) {
+		if (max < 0) max = 0;
+		if (current > max) current = max;
+		if (current < 0) current = 0;
+
+		this.resources[key.toUpperCase()] = {
+			"current": current,
+			"max": max,
+			"regen": regen
+		};
+	};
+
+	behinstProto.onDestroy = function ()
+	{
+		// called when associated object is being destroyed
+		// note runtime may keep the object and behavior alive after this call for recycling;
+		// release, recycle or reset any references here as necessary
+	};
+	
+	// called when saving the full state of the game
+	behinstProto.saveToJSON = function ()
+	{
+		// return a Javascript object containing information about your behavior's state
+		return {
+			"res": this.resources
+		};
+	};
+	
+	// called when loading the full state of the game
+	behinstProto.loadFromJSON = function (o)
+	{
+		this.resources = o["res"];
+	};
+
+	behinstProto.tick = function ()
+	{
+		var dt = this.runtime.getDt(this.inst);
+		if (dt === 0) return;
+
+		for (var key in this.resources) {
+			if (this.resources.hasOwnProperty(key)) {
+				var res = this.resources[key];
+				if (res.regen !== 0) {
+					res.current += res.regen * dt;
+					// Clamp the value between 0 and max
+					res.current = cr.clamp(res.current, 0, res.max);
+				}
+			}
+		}
+	};
+	
+	// The comments around these functions ensure they are removed when exporting, since the
+	// debugger code is no longer relevant after publishing.
+	/**BEGIN-PREVIEWONLY**/
+	behinstProto.getDebuggerValues = function (propsections)
+	{
+		// Append to propsections any debugger sections you want to appear.
+		// Each section is an object with two members: "title" and "properties".
+		var debugger_props = [];
+		for (var key in this.resources)
+		{
+			if (this.resources.hasOwnProperty(key))
+			{
+				var res = this.resources[key];
+				debugger_props.push({"name": key, "value": res.current.toFixed(2) + " / " + res.max + " (" + res.regen + "/s)", "readonly": true});
+			}
+		}
+
+		propsections.push({
+			"title": this.type.name,
+			"properties": debugger_props
+		});
+	};
+	
+	behinstProto.onDebugValueEdited = function (header, name, value)
+	{
+		// Not used, as all debugger values are read-only.
+	};
+	/**END-PREVIEWONLY**/
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	Cnds.prototype.OnHealthDepleted = function ()
+	{
+		return true; // Triggered by an action
+	};
+
+	Cnds.prototype.IsResourceMaxed = function (key)
+	{
+		key = key.toUpperCase();
+		var res = this.resources[key];
+		if (!res) return false;
+		return res.current >= res.max;
+	};
+
+	Cnds.prototype.IsResourceAvailable = function (key, required)
+	{
+		key = key.toUpperCase();
+		var res = this.resources[key];
+		if (!res) return false;
+		return res.current >= required;
+	};
+	
+	Cnds.prototype.OnManaDepleted = function ()
+	{
+		return true; // Triggered by an action
+	};
+	
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	// the example action
+	Acts.prototype.TakeDamage = function (amount)
+	{
+		behaviorProto.acts.ModifyResource.call(this, "HP", -amount);
+	};
+
+	Acts.prototype.Heal = function (amount)
+	{
+		behaviorProto.acts.ModifyResource.call(this, "HP", amount);
+	};
+
+	Acts.prototype.ModifyResource = function (key, amount)
+	{
+		// Ensure key is a string to prevent .toUpperCase() on a non-string.
+		if (typeof key !== "string")
+			return;
+
+		key = key.toUpperCase();
+		var res = this.resources[key];
+		if (!res) return;
+
+		var oldValue = res.current;
+		res.current += amount;
+		res.current = cr.clamp(res.current, 0, res.max);
+
+		// If HP was modified and dropped to 0, trigger the condition
+		if (oldValue > 0 && res.current <= 0)
+		{
+			if (key === "HP") {
+				this.runtime.trigger(cr.behaviors.RpgPoints.prototype.cnds.OnHealthDepleted, this.inst);
+			}
+			else if (key === "MP") {
+				this.runtime.trigger(cr.behaviors.RpgPoints.prototype.cnds.OnManaDepleted, this.inst);
+			}
+		}
+	};
+
+	Acts.prototype.SetMaxResource = function (key, newMax)
+	{
+		key = key.toUpperCase();
+		var res = this.resources[key];
+		if (!res) return;
+
+		res.max = (newMax < 0) ? 0 : newMax;
+		// Clamp current value to new max
+		res.current = cr.clamp(res.current, 0, res.max);
+	};
+
+	Acts.prototype.AddCustomResource = function (key, max, current, regen)
+	{
+		this.addResource(key, max, current, regen);
+	};
+
+	Acts.prototype.SpendMana = function (amount)
+	{
+		behaviorProto.acts.ModifyResource.call(this, "MP", -amount);
+	};
+
+	Acts.prototype.RestoreMana = function (amount)
+	{
+		behaviorProto.acts.ModifyResource.call(this, "MP", amount);
+	};
+
+	behaviorProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+
+	// the example expression
+	Exps.prototype.CurrentResource = function (ret, key)
+	{
+		key = key.toUpperCase();
+		var res = this.resources[key];
+		ret.set_float(res ? res.current : 0);
+	};
+
+	Exps.prototype.MaxResource = function (ret, key)
+	{
+		key = key.toUpperCase();
+		var res = this.resources[key];
+		ret.set_float(res ? res.max : 0);
+	};
+
+	Exps.prototype.ResourcePercent = function (ret, key)
+	{
+		key = key.toUpperCase();
+		var res = this.resources[key];
+		if (res && res.max > 0) {
+			ret.set_float((res.current / res.max) * 100.0);
+		} else {
+			ret.set_float(0);
+		}
+	};
+
+	// HP Expressions
+	Exps.prototype.HP = function (ret)
+	{
+		behaviorProto.exps.CurrentResource.call(this, ret, "HP");
+	};
+	Exps.prototype.MaxHP = function (ret)
+	{
+		behaviorProto.exps.MaxResource.call(this, ret, "HP");
+	};
+	Exps.prototype.HPPercent = function (ret)
+	{
+		behaviorProto.exps.ResourcePercent.call(this, ret, "HP");
+	};
+
+	// MP Expressions
+	Exps.prototype.MP = function (ret)
+	{
+		behaviorProto.exps.CurrentResource.call(this, ret, "MP");
+	};
+	Exps.prototype.MaxMP = function (ret)
+	{
+		behaviorProto.exps.MaxResource.call(this, ret, "MP");
+	};
+	Exps.prototype.MPPercent = function (ret)
+	{
+		behaviorProto.exps.ResourcePercent.call(this, ret, "MP");
+	};
+
+	behaviorProto.exps = new Exps();
+	
+}());
