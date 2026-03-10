a3dae98c328e612e8c31510848cc5f751c885818
pigmalien <japigg@bellsouth.net>
2026-03-10 14:42:42 -0400

    Add HTML Menu plugin with core functionality and UI elements
    - Created common.js for shared scripts between IDE and runtime.
    - Implemented edittime.js to define plugin settings, parameters, conditions, actions, and expressions.
    - Added menu.html for the interactive menu layout with buttons and dynamic content areas.
    - Developed runtime.js to handle plugin logic, including loading HTML/CSS, event handling, and data synchronization.
    - Introduced style.css for menu styling, ensuring a visually appealing and functional interface.

---
+++ plugins/htmlmenu/edittime.js
@@ -0,0 +1,147 @@
+﻿﻿function GetPluginSettings()
+{
+	return {
+		"name":			"HTML Menu",			// as appears in 'insert object' dialog, can be changed as long as "id" stays the same
+		"id":			"HTMLMenu",				// this is used to identify this plugin and is saved to the project; never change it
+		"version":		"1.0",					// (float in x.y format) Plugin version - C2 shows compatibility warnings based on this
+		"description":	"Displays an interactive HTML/CSS element over the game canvas.",
+		"author":		"Gemini Code Assist",
+		"help url":		"https://www.construct.net",
+		"category":		"User interface",		// Prefer to re-use existing categories, but you can set anything here
+		"type":			"world",				// either "world" (appears in layout and is drawn), else "object"
+		"rotatable":	false,					// only used when "type" is "world".  Enables an angle property on the object.
+		"flags":		pf_position_aces		// compare/set/get x, y...
+					| pf_size_aces			// compare/set/get width, height...
+					| pf_appearance_aces	// compare/set/get visible, opacity...
+					//	| pf_singleglobal		// exists project-wide, e.g. mouse, keyboard.  "type" must be "object".
+					//	| pf_texture			// object has a single texture (e.g. tiled background)
+					//	| pf_animations			// enables the animations system.  See 'Sprite' for usage
+					//	| pf_zorder_aces		// move to top, bottom, layer...
+					//  | pf_nosize				// prevent resizing in the editor
+					//	| pf_effects			// allow WebGL shader effects to be added
+					//  | pf_predraw			// set for any plugin which draws and is not a sprite (i.e. does not simply draw
+												// a single non-tiling image the size of the object) - required for effects to work properly
+	};
+};
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])			// a number
+// AddStringParam(label, description [, initial_string = "\"\""])		// a string
+// AddAnyTypeParam(label, description [, initial_string = "0"])			// accepts either a number or string
+// AddCmpParam(label, description)										// combo with equal, not equal, less, etc.
+// AddComboParamOption(text)											// (repeat before "AddComboParam" to add combo items)
+// AddComboParam(label, description [, initial_selection = 0])			// a dropdown list parameter
+// AddObjectParam(label, description)									// a button to click and pick an object type
+// AddLayerParam(label, description)									// accepts either a layer number or name (string)
+// AddLayoutParam(label, description)									// a dropdown list with all project layouts
+// AddKeybParam(label, description)										// a button to click and press a key (returns a VK)
+// AddAnimationParam(label, description)								// a string intended to specify an animation name
+// AddAudioFileParam(label, description)								// a dropdown list with all imported project audio files
+
+////////////////////////////////////////
+// Conditions
+
+AddCondition(0, cf_trigger, "On button clicked", "Menu", "On button clicked", "Triggered when a menu element with 'data-c2-id' is clicked.", "OnButtonClicked");
+
+AddCondition(1, cf_trigger, "On sound triggered", "Sound", "On sound triggered", "Triggered when an HTML element with a 'data-sfx' attribute is interacted with.", "OnSoundTriggered");
+
+////////////////////////////////////////
+// Actions
+
+AddStringParam("Element ID", "The ID of the element to update.");
+AddAnyTypeParam("Content", "The new content for the element.");
+AddAction(1, af_none, "Update element content", "Content", "Set content of element with ID <b>{0}</b> to <i>{1}</i>", "Update the innerHTML of a specific element by its ID.", "UpdateContent");
+
+AddAction(2, af_none, "Force sync now", "Sync", "Force universal sync", "Manually trigger the universal sync to update all linked HTML elements.", "ForceSyncNow");
+
+AddStringParam("Dictionary JSON", "The JSON data from a Dictionary object (e.g., Dictionary.AsJSON).");
+AddAction(3, af_none, "Sync from Dictionary", "Sync", "Sync from Dictionary JSON <i>{0}</i>", "Load data from a Dictionary's JSON to be used by the universal sync.", "SyncFromDictionary");
+
+////////////////////////////////////////
+// Expressions
+
+AddExpression(0, ef_return_string, "ClickedID", "Menu", "ClickedID", "Get the 'data-c2-id' value from the last clicked button.");
+
+AddExpression(1, ef_return_string, "LastSFX", "Sound", "LastSFX", "Get the name of the sound effect triggered from the HTML.");
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+// Array of property grid properties for this plugin
+
+var property_list = [
+	new cr.Property(ept_text,		"HTML file",		"menu.html",	"Filename of the HTML content to load (must be in project files)."),
+	new cr.Property(ept_text,		"CSS file",			"style.css",	"Filename of the CSS stylesheet to load (must be in project files)."),
+	new cr.Property(ept_combo,		"Initially visible", "Yes",			"Whether the menu is visible at the start of the layout.", "No|Yes"),
+	new cr.Property(ept_combo,		"Auto-Sync", 		"Yes",			"Automatically sync data from C2 variables to HTML elements each tick.", "No|Yes"),
+	new cr.Property(ept_combo,		"Two-Way Binding",	"No",			"Enable syncing changes from HTML input elements back to C2 global variables.", "No|Yes")
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	// this.myValue = 0...
+}
+
+// Called when inserted via Insert Object Dialog for the first time
+IDEInstance.prototype.OnInserted = function()
+{
+}
+
+// Called when double clicked in layout
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called after a property has been changed in the properties bar
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+
+// For rendered objects to load fonts or textures
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+
+// Called to draw self in the editor if a layout object
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// For rendered objects to release fonts or textures
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
---
+++ plugins/htmlmenu/runtime.js
@@ -0,0 +1,520 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+//          vvvvvvvv
+cr.plugins_.HTMLMenu = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+	//                            vvvvvvvv
+	var pluginProto = cr.plugins_.HTMLMenu.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		
+		// any other properties you need, e.g...
+		// this.myValue = 0;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+		this.htmlFile = this.properties[0];
+		this.cssFile = this.properties[1];
+		this.visible = (this.properties[2] === 1); // 0=No, 1=Yes
+		this.autoSync = (this.properties[3] === 1); // 0=No, 1=Yes
+		this.twoWayBinding = (this.properties[4] === 1); // 0=No, 1=Yes
+		this.activeInput = null; // For two-way binding loop prevention
+		this.syncData = {}; // For "Sync from Dictionary" action
+		this.lastSFX = ""; // For Sound Bridge
+
+		// Create the DOM element
+		this.elem = document.createElement("div");
+		this.elem.id = "c2-htmlmenu-" + this.uid;
+		this.elem.style.pointerEvents = "none"; // Container should not block input
+		this.elem.style.imageRendering = "pixelated"; // Ensure crisp rendering for pixel art games
+
+		// Append to the document body to use absolute page coordinates for positioning
+		document.body.appendChild(this.elem);
+		this.jQueryElem = jQuery(this.elem);
+
+		// Load external CSS file
+		this.cssLink = null;
+		if (this.cssFile)
+		{
+			this.cssLink = document.createElement("link");
+			this.cssLink.rel = "stylesheet";
+			this.cssLink.type = "text/css";
+			this.cssLink.href = this.cssFile;
+			document.getElementsByTagName('head')[0].appendChild(this.cssLink);
+		}
+
+		// Load external HTML file via AJAX
+		if (this.htmlFile)
+		{
+			var self = this;
+			
+			// Define an error handler to show clear visual feedback in the layout
+			var onError = function(status) {
+				self.elem.style.backgroundColor = "red";
+				self.elem.style.color = "white";
+				self.elem.style.fontFamily = "sans-serif";
+				self.elem.style.padding = "10px";
+				self.elem.style.boxSizing = "border-box";
+				self.elem.innerHTML = "<strong>HTMLMenu Error:</strong><br>Failed to load '" + self.htmlFile + "'.<br>Status: " + status + "<br>Check filename and ensure it's in the project 'Files' folder. Check browser console (F12) for more details.";
+				console.error("HTMLMenu: Error loading '" + self.htmlFile + "'. Status: " + status);
+			};
+
+			var request = new XMLHttpRequest();
+			request.open("GET", this.htmlFile, true);
+			request.onload = function () {
+				if (request.status >= 200 && request.status < 400) {
+					self.elem.innerHTML = request.responseText;
+					
+					// Tier 3: Manually execute any <script> tags found in the injected HTML
+					var scripts = self.elem.getElementsByTagName("script");
+					for (var i = 0; i < scripts.length; i++) {
+						var script = document.createElement("script");
+						if (scripts[i].src) script.src = scripts[i].src;
+						else script.text = scripts[i].text;
+						// Append to body to execute, then remove to keep DOM clean
+						document.body.appendChild(script).parentNode.removeChild(script);
+					}
+				} else {
+					// File found, but server returned an error (e.g. 404 Not Found, 500)
+					onError(request.status);
+				}
+			};
+			request.onerror = function () { onError("Network Error"); }; // Network errors (e.g. CORS, no connection)
+			request.send();
+		}
+
+		// Tier 2: JS-to-Event-Sheet Bridge (Delegated Listener)
+		this.lastClickedID = "";
+		var self = this;
+		this.jQueryElem.on("click", "[data-c2-id]", function (e) {
+			e.stopPropagation(); // Prevent event from bubbling to other listeners
+			self.lastClickedID = jQuery(this).attr("data-c2-id");
+			self.runtime.trigger(cr.plugins_.HTMLMenu.prototype.cnds.OnButtonClicked, self);
+		});
+
+		// Tier 5: Sound Bridge (HTML -> C2 SFX)
+		// Delegated listener for click sounds. Looks for 'data-sfx-click' attribute.
+		this.jQueryElem.on("mousedown", "[data-sfx-click]", function(e) {
+			e.stopPropagation(); // Prevent event from bubbling up further
+			self.lastSFX = jQuery(this).attr("data-sfx-click");
+			self.runtime.trigger(cr.plugins_.HTMLMenu.prototype.cnds.OnSoundTriggered, self);
+		});
+
+		// Delegated listener for hover sounds. Looks for 'data-sfx-hover' attribute.
+		this.jQueryElem.on("mouseenter", "[data-sfx-hover]", function(e) {
+			// Stop propagation is less critical for mouseenter but good for consistency.
+			e.stopPropagation();
+			self.lastSFX = jQuery(this).attr("data-sfx-hover");
+			self.runtime.trigger(cr.plugins_.HTMLMenu.prototype.cnds.OnSoundTriggered, self);
+		});
+
+		// Tier 4: Two-Way Data Binding (HTML -> C2)
+		if (this.twoWayBinding) {
+			var self = this;
+
+			// Set a flag when an input is focused to prevent forward-sync from overwriting it while typing.
+			this.jQueryElem.on("focus", "input[id], textarea[id], select[id]", function(e) {
+				self.activeInput = e.target;
+			});
+
+			// On blur, clear the flag. The final value is already set by the 'input' event.
+			// We use 'focusout' because it bubbles, unlike 'blur', making it suitable for delegated events.
+			this.jQueryElem.on("focusout", "input[id], textarea[id], select[id]", function(e) {
+				if (self.activeInput === e.target) {
+					self.activeInput = null;
+				}
+			});
+
+			// Listener for the actual data change.
+			this.jQueryElem.on("input", "input[id], textarea[id], select[id]", function(e) {
+				var target = e.target;
+				var varName = target.id;
+				if (!varName) return;
+
+				// Find and update the corresponding C2 global variable.
+				var gv = self.getGlobalVarByName(varName);
+				if (gv) {
+					var value = target.value;
+					var num = Number(value); // Use stricter Number() conversion
+
+					// Convert to number if it's a valid number string, otherwise keep as string.
+					// Handle empty strings explicitly to avoid them becoming 0.
+					if (value.trim() === "") {
+						gv.data = ""; // C2 will treat this as 0 for number variables.
+					} else if (!isNaN(num) && isFinite(num)) {
+						gv.data = num;
+					} else {
+						gv.data = value;
+					}
+				}
+			});
+		}
+
+		// Prevent clicks on the menu from "bleeding through" to the game canvas
+		this.jQueryElem.on("mousedown touchstart", function (e) {
+			e.stopPropagation();
+		});
+
+		// For tracking changes to avoid unnecessary DOM updates
+		this.last_visible = !this.visible;
+		this.last_left = -1;
+		this.last_top = -1;
+		this.last_w = -1;
+		this.last_h = -1;
+		this.last_opacity = -1;
+
+		this.runtime.tickMe(this);
+	};
+	
+	// called whenever an instance is destroyed
+	// note the runtime may keep the object after this call for recycling; be sure
+	// to release/recycle/reset any references to other objects in this function.
+	instanceProto.onDestroy = function ()
+	{
+		if (this.jQueryElem)
+			this.jQueryElem.remove();
+		this.elem = null;
+		this.jQueryElem = null;
+
+		if (this.cssLink)
+			jQuery(this.cssLink).remove();
+		this.cssLink = null;
+	};
+	
+	// called when saving the full state of the game
+	instanceProto.saveToJSON = function ()
+	{
+		return {
+			// e.g.
+			//"myValue": this.myValue
+		};
+	};
+	
+	// called when loading the full state of the game
+	instanceProto.loadFromJSON = function (o)
+	{
+	};
+
+	/**
+	 * Safely finds an element within the plugin's div and executes a callback.
+	 * This is the requested "SafeUpdate" function.
+	 * @param {string} elemId The ID of the element to find.
+	 * @param {function(HTMLElement)} callback The function to execute with the found element.
+	 * @returns {boolean} True if the element was found and the callback was executed, otherwise false.
+	 */
+	instanceProto.safeUpdateElement = function (elemId, callback) {
+		if (!this.elem || !elemId || !callback) return false;
+		const target = this.elem.querySelector("#" + elemId);
+		if (target) {
+			callback(target);
+			return true;
+		}
+		return false;
+	};
+
+	/**
+	 * Helper to find a C2 global variable object by its name.
+	 * @param {string} name The name of the global variable.
+	 * @returns {object|null} The global variable object {name, data, ...} or null if not found.
+	 */
+	instanceProto.getGlobalVarByName = function (name) {
+		if (this.runtime.all_global_vars && this.runtime.all_global_vars.length) {
+			for (var i = 0, len = this.runtime.all_global_vars.length; i < len; i++) {
+				var gv = this.runtime.all_global_vars[i];
+				if (gv.name === name) return gv;
+			}
+		}
+		return null;
+	};
+
+	/**
+	 * Universal Sync system. Finds elements with 'id' or 'data-sync-id' and populates them
+	 * with data from C2.
+	 */
+	instanceProto.doUniversalSync = function () {
+		if (!this.elem) return;
+
+		// Create a key-value map of global variables for efficient lookup.
+		// this.runtime.all_global_vars is the correct C2 SDK way to access them.
+		var gvars_map = {};
+		if (this.runtime.all_global_vars && this.runtime.all_global_vars.length) {
+			for (var j = 0, len = this.runtime.all_global_vars.length; j < len; j++) {
+				var gv = this.runtime.all_global_vars[j];
+				gvars_map[gv.name] = gv.data;
+			}
+		}
+
+		// Find all elements that can be synced
+		const elements = this.elem.querySelectorAll('[id], [data-sync-id]');
+		
+		for (let i = 0; i < elements.length; i++) {
+			const elem = elements[i];
+			const syncKey = elem.getAttribute('data-sync-id') || elem.id;
+
+			// Two-way binding: Don't sync forward to the element the user is currently typing in.
+			if (elem === this.activeInput) continue;
+			if (!syncKey) continue;
+
+			let value = undefined;
+
+			// Priority 1: Data from the "Sync from Dictionary" action.
+			if (this.syncData.hasOwnProperty(syncKey)) {
+				value = this.syncData[syncKey];
+			}
+			// Priority 2: C2 Global Variables.
+			else if (gvars_map.hasOwnProperty(syncKey)) {
+				value = gvars_map[syncKey];
+			}
+
+			if (value !== undefined) {
+				let formattedValue = value;
+				const format = elem.getAttribute('data-format');
+				if (format && typeof value === 'number') {
+					if (format === 'percent') {
+						formattedValue = (value * 100).toFixed(0) + '%';
+					} else if (!isNaN(parseFloat(format))) { // e.g. "0.00"
+						const decimalPlaces = (format.split('.')[1] || '').length;
+						formattedValue = value.toFixed(decimalPlaces);
+					}
+				}
+				
+				// Use .value for form elements, .innerHTML for others.
+				var isInput = (elem.tagName === "INPUT" || elem.tagName === "TEXTAREA" || elem.tagName === "SELECT");
+				if (isInput) {
+					if (elem.value !== String(formattedValue)) {
+						elem.value = formattedValue;
+					}
+				} else {
+					if (elem.innerHTML !== String(formattedValue)) { // Simple optimization
+						elem.innerHTML = formattedValue;
+					}
+				}
+			}
+		}
+	};
+
+	instanceProto.tick = function ()
+	{
+		// Handle visibility from C2's 'Set visible' action
+		var vis = this.visible;
+		if (this.last_visible !== vis) {
+			this.jQueryElem.css("display", vis ? "block" : "none");
+			this.last_visible = vis;
+		}
+
+		if (!vis) {
+			return;
+		}
+
+		// Auto-Sync polling
+		if (this.autoSync) {
+			this.doUniversalSync();
+		}
+
+		// Get object's top-left and bottom-right in layout co-ordinates, accounting for hotspot
+		var lx = this.x - (this.hotspotX * this.width);
+		var ly = this.y - (this.hotspotY * this.height);
+		var lr = lx + this.width;
+		var lb = ly + this.height;
+
+		// Convert to canvas co-ordinates
+		var left = this.layer.layerToCanvas(lx, ly, true);
+		var top = this.layer.layerToCanvas(lx, ly, false);
+		var right = this.layer.layerToCanvas(lr, lb, true);
+		var bottom = this.layer.layerToCanvas(lr, lb, false);
+
+		// Is entirely offscreen or invisible: hide
+		if (!this.layer.visible || right <= 0 || bottom <= 0 || left >= this.runtime.width || top >= this.runtime.height)
+		{
+			this.jQueryElem.hide();
+			return;
+		}
+
+		this.jQueryElem.show();
+
+		var canvas_offset = jQuery(this.runtime.canvas).offset();
+		if (!canvas_offset) return;
+
+		var offx = left + canvas_offset.left;
+		var offy = top + canvas_offset.top;
+		var w = right - left;
+		var h = bottom - top;
+		var opacity = this.opacity;
+
+		// Optimization: check if anything changed to avoid DOM thrashing
+		if (this.last_left === offx && this.last_top === offy && this.last_w === w && this.last_h === h && this.last_opacity === opacity) {
+			return;
+		}
+
+		this.jQueryElem.offset({left: Math.round(offx), top: Math.round(offy)});
+		this.jQueryElem.width(Math.round(w));
+		this.jQueryElem.height(Math.round(h));
+
+		// Cache the last values
+		this.last_left = offx;
+		this.last_top = offy;
+		this.last_w = w;
+		this.last_h = h;
+		this.last_opacity = opacity;
+	};
+	
+	// only called if a layout object - draw to a canvas 2D context
+	instanceProto.draw = function(ctx)
+	{
+	};
+	
+	// only called if a layout object in WebGL mode - draw to the WebGL context
+	// 'glw' is not a WebGL context, it's a wrapper - you can find its methods in GLWrap.js in the install
+	// directory or just copy what other plugins do.
+	instanceProto.drawGL = function (glw)
+	{
+	};
+	
+	// The comments around these functions ensure they are removed when exporting, since the
+	// debugger code is no longer relevant after publishing.
+	/**BEGIN-PREVIEWONLY**/
+	instanceProto.getDebuggerValues = function (propsections)
+	{
+		// Append to propsections any debugger sections you want to appear.
+		// Each section is an object with two members: "title" and "properties".
+		// "properties" is an array of individual debugger properties to display
+		// with their name and value, and some other optional settings.
+		propsections.push({
+			"title": "My debugger section",
+			"properties": [
+				// Each property entry can use the following values:
+				// "name" (required): name of the property (must be unique within this section)
+				// "value" (required): a boolean, number or string for the value
+				// "html" (optional, default false): set to true to interpret the name and value
+				//									 as HTML strings rather than simple plain text
+				// "readonly" (optional, default false): set to true to disable editing the property
+				
+				// Example:
+				// {"name": "My property", "value": this.myValue}
+			]
+		});
+	};
+	
+	instanceProto.onDebugValueEdited = function (header, name, value)
+	{
+		// Called when a non-readonly property has been edited in the debugger. Usually you only
+		// will need 'name' (the property name) and 'value', but you can also use 'header' (the
+		// header title for the section) to distinguish properties with the same name.
+		if (name === "My property")
+			this.myProperty = value;
+	};
+	/**END-PREVIEWONLY**/
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	
+	Cnds.prototype.OnButtonClicked = function ()
+	{
+		return true; // It's a trigger
+	};
+	
+	Cnds.prototype.OnSoundTriggered = function ()
+	{
+		return true; // It's a trigger
+	};
+	
+	pluginProto.cnds = new Cnds();
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	
+	Acts.prototype.UpdateContent = function (elemId, content)
+	{
+		// Use the new safeUpdateElement helper function
+		const success = this.safeUpdateElement(elemId, function(element) {
+			element.innerHTML = content;
+		});
+
+		if (!success) {
+			console.warn("HTMLMenu: Element with ID '" + elemId + "' not found.");
+		}
+	};
+
+	Acts.prototype.ForceSyncNow = function ()
+	{
+		this.doUniversalSync();
+	};
+
+	Acts.prototype.SyncFromDictionary = function (jsonString)
+	{
+		if (!jsonString) {
+			this.syncData = {};
+			return;
+		}
+		try {
+			var parsed = JSON.parse(jsonString);
+			// Handle C2's Dictionary.AsJSON format which wraps data in a "data" property
+			if (parsed && parsed.c2dictionary && parsed.hasOwnProperty("data")) {
+				this.syncData = parsed.data;
+			} else {
+				// Fallback for raw JSON objects if not from a C2 dictionary
+				this.syncData = parsed;
+			}
+		} catch (e) {
+			console.error("HTMLMenu: Invalid JSON string provided for 'Sync from Dictionary'.", e);
+		}
+	};
+	
+	pluginProto.acts = new Acts();
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	Exps.prototype.ClickedID = function (ret)
+	{
+		ret.set_string(this.lastClickedID);
+	};
+	
+	Exps.prototype.LastSFX = function (ret)
+	{
+		ret.set_string(this.lastSFX);
+	};
+	
+	pluginProto.exps = new Exps();
+
+}());
